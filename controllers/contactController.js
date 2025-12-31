78aca7ad1ecf3437c9377235ec42856d2e99e80d
Akshay-singh-goyal <152581106+Akshay-singh-goyal@users.noreply.github.com>
2025-12-31 15:19:12 +0530

    Create contactController.js

---
+++ controllers/contactController.js
@@ -0,0 +1,34 @@
+const Contact = require("../models/Contact");
+
+exports.createContact = async (req, res) => {
+  try {
+    const { name, email, category, priority, subject, message } = req.body;
+
+    if (!name || !email || !subject) {
+      return res.status(400).json({
+        success: false,
+        message: "Required fields missing",
+      });
+    }
+
+    const contact = await Contact.create({
+      name,
+      email,
+      category,
+      priority,
+      subject,
+      message,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Message received successfully",
+      data: contact,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
