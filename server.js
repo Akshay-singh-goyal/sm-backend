4745ffcce8894813fa6f2954190365fed1362ad5
Akshay-singh-goyal <152581106+Akshay-singh-goyal@users.noreply.github.com>
2026-01-01 15:59:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,14 +29,14 @@ const registerRoutes = require("./routes/registerRoutes");
 const contactRoutes = require("./routes/contactRoutes");
 const newsletterRoutes = require("./routes/newsletterRoutes");
 const adminRoutes = require("./routes/adminRegistration.routes");
-import adminRegistrationRoutes from "./routes/adminRegistrationRoutes.js.js";
+import adminRegistrationRoutes from "./routes/adminRegistrationRoutes.js";
 
 app.use("/api/auth", authRoutes);
 app.use("/api/register", registerRoutes);
 app.use("/api", contactRoutes);
 app.use("/api/newsletter", newsletterRoutes);
 app.use("/api/admin", adminRoutes);
-app.use("/admin", adminRegistrationRoutes)
+app.use("/api/admin/registrations", adminRegistrationRoutes); // registrations
 
 /* ===== Health Check ===== */
 app.get("/", (req, res) => {
