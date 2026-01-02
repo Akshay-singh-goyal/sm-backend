f7d34259998e6cfc7503dd3a7fa6d7ac7a1f48af
Akshay-singh-goyal <152581106+Akshay-singh-goyal@users.noreply.github.com>
2026-01-02 10:19:15 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
-  } catch (err) {
-    console.log("DB Error:", err.message);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
