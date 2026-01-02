ccc5e17a082d302496c7836aa05eade17822134d
Akshay-singh-goyal <152581106+Akshay-singh-goyal@users.noreply.github.com>
2026-01-02 10:32:20 +0530

    Update Newsletter.js

--- models/Newsletter.js
+++ models/Newsletter.js
@@ -1,8 +1,9 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const subscriberSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model("Subscriber", subscriberSchema);
+const Subscriber = mongoose.model("Subscriber", subscriberSchema);
+export default Subscriber;
